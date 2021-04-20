5974ff238d5ecfa0277be66b36cbdd7dadbb0198
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-04-20 19:55:15 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,32 @@
+canvas=document.getElementById("myCanvas");
+color="purple";
+
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+
+canvas.addEventListener("mousedown", my_Mousedown);
+function my_Mousedown(e){
+    color=document.getElementById("color").value;
+mouse_X=e.clientX-canvas.offsetLeft;
+mouse_Y=e.clientY-canvas.offsetTop;
+circle(mouse_X, mouse_Y);
+
+}
+function circle(mouse_X, mouse_Y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_X, mouse_Y, 40,0,2*Math.PI);
+    ctx.stroke();
+}
+function clearArea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+
+
